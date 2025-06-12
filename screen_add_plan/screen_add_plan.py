dea2543919c6664353a807b60e8fa71a539a8a10
AnsonGit-MakieTech <techmakie@gmail.com>
2025-06-12 17:10:35 +0800

    update git ignore

--- screen_add_plan/screen_add_plan.py
+++ screen_add_plan/screen_add_plan.py
@@ -19,6 +19,23 @@ from kivy.uix.behaviors import ButtonBehavior
 from variables import *
 import os
 
+from kivy_garden.mapview import MapView, MapSource  # Make sure mapview is installed
+
+# Optional: Custom tile server or use default
+map_source_labeled = MapSource(url="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                       cache_key="osm",
+                       tile_size=256,
+                       image_ext="png")
+map_source_satlite = MapSource(
+    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    cache_key="satellite",
+    tile_size=256,
+    image_ext="jpg",  # Esri tiles are usually JPG
+    attribution="Tiles © Esri — Source: Esri, Earthstar Geographics",
+    max_zoom = 17, 
+    min_zoom = 5
+)
+
 
 
 class ScreenAddPlan(Screen):
