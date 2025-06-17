9870b221faaea53c11125de99e31928173a43dbf
AnsonGit-MakieTech <techmakie@gmail.com>
2025-06-17 15:25:32 +0800

    fix ui in main screen

--- screen_first_time/screen_first_time.py
+++ screen_first_time/screen_first_time.py
@@ -26,12 +26,30 @@ from screen_components import app_button, top_form_buttons, text_input
 class FirstTimeScreen(Screen): 
     logo = StringProperty('')
     image_size = NumericProperty(100)
+    visit_icon = StringProperty('')
+    visit_icon_size = NumericProperty(100)
+    charles_tv_icon_spacing = NumericProperty(10)
+    has_wifi_icon = StringProperty('')
+    register_icon = StringProperty('')
+    button_icon_size = NumericProperty(100)
+
+
+    h1_font_size = NumericProperty(30)
+    h2_font_size = NumericProperty(20)
+    h3_font_size = NumericProperty(15)
+    h4_font_size = NumericProperty(12)
+
+    button_spacing = NumericProperty(20)
+    button_size = NumericProperty(200)
+    
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         parent_dir = os.path.dirname(os.path.dirname(__file__))
         self.logo = os.path.join(parent_dir, 'assets', 'app_logo.png')
-
+        self.visit_icon = os.path.join(parent_dir, 'assets', 'visit_icon.png')
+        self.has_wifi_icon = os.path.join(parent_dir, 'assets', 'has_wifi_icon.png')
+        self.register_icon = os.path.join(parent_dir, 'assets', 'register_icon.png')
  
 
     def on_parent(self, instance, parent):
@@ -46,9 +64,20 @@ class FirstTimeScreen(Screen):
  
     def update_sizing(self, *args):
         width , height = self.size
-        self.image_size = min(width, height) * 0.45
-        # if self.image_size > 200:
-        #     self.image_size = 200
+        self.image_size = min(width, height) * 0.45 
+        self.button_icon_size = min(width, height) * 0.12
+
+        self.h1_font_size = int(min(width, height) * 0.038)
+        self.h2_font_size = int(min(width, height) * 0.033)
+        self.h3_font_size = int(min(width, height) * 0.025)
+        self.h4_font_size = int(min(width, height) * 0.023)
+
+        self.button_spacing = int(min(width, height) * 0.08)
+        self.button_size = int(min(width, height) * 0.3)
+
+        self.visit_icon_size = int(min(width, height) * 0.06)
+        self.charles_tv_icon_spacing = int(min(width, height) * 0.02)
+
 
         
         print(f'width: {width}, height: {height} , image_size: {self.image_size}')
